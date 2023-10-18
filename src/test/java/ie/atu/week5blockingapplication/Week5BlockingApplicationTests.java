6e80141dbf0923c979d70533a72c8355dcc5923f
dewanhasan99 <g00410156@atu.ie>
2023-10-18 14:44:01 +0100

    First Commit

---
+++ src/test/java/ie/atu/week5blockingapplication/Week5BlockingApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week5blockingapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week5BlockingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
